c8d19169e79f8eeea049e45c1ba77c8569a46237
MathiasKLarsen <mathiaskrogkaer@gmail.com>
2025-10-23 19:51:07 +0200

    about start

--- src/app/admin/dashboard/about/page.jsx
+++ src/app/admin/dashboard/about/page.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
+"use client"
+
+import { useState, useEffect } from 'react'
+import { getAbout } from '../../../../data/getAbout';
 
 const page = () => {
+
+  const [aboutData, setAboutData] = useState(null);
+
+  useEffect(() => {
+    getAbout().then(setAboutData);
+  }, []);
+
   return (
     <>
       <h2>About</h2>
-      <ol className='grid grid-cols-4 gap-4 mt-4'>
-        <li className='bg-amber-300'>Edit</li>
-      </ol>
+      
+      <article className='flex justify-center'>
+        {aboutData && (
+          <section className="grid grid-cols-2 h-[500px] w-[800px]">
+            <div className='border p-5'>
+              <p className="text-2xl text-left">{aboutData.content1}</p>
+              <button
+                  type="button"
+                  className="cursor-pointer bg-blue-400 py-2 px-4 rounded"
+                  onClick={() => {
+                    setEditProduct(item);
+                    setShowModal(true);
+                  }}
+                >
+                  Edit Product
+                </button>
+            </div>
+
+            <div className='border p-5'>
+              <p className="text-2xl text-left">{aboutData.content2}</p>
+              <button
+                  type="button"
+                  className="cursor-pointer bg-blue-400 py-2 px-4 rounded"
+                  onClick={() => {
+                    setEditProduct(item);
+                    setShowModal(true);
+                  }}
+                >
+                  Edit Product
+                </button>
+            </div>
+          </section>
+        )}
+      </article>
     </>
   )
 }
--- src/data/getAbout.js
+++ src/data/getAbout.js
@@ -1,5 +1,17 @@
+const apiUrl = "http://localhost:5039/about";
+
 export async function getAbout() {
-    const res = await fetch(`http://localhost:5039/about`);
+    const res = await fetch(`${apiUrl}`);
     if (!res.ok) throw new Error("Failed to fetch product");
     return res.json();
+}
+
+export async function updateProduct(id, data) {
+  const res = await fetch(`${apiUrl}/admin/${id}`, {
+    method: "PUT",
+    body: data,
+  });
+
+  if (!res.ok) throw new Error("Failed to update product");
+  return res.json();
 }
